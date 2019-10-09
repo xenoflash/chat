bf1f0208747a305f91590a113066f95787cf2108
ragini <ragini.yadav@iongroup.com>
2019-10-09 13:33:58 +0530

    kafka + websockets in progress.

---
+++ src/main/java/com/lf/chat/controller/ChattingController.java
@@ -0,0 +1,32 @@
+package com.lf.chat.controller;
+
+import com.lf.chat.model.ChattingMessage;
+import com.lf.chat.kafka.consumer.Receiver;
+import com.lf.chat.kafka.producer.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChattingController {
+
+    @Autowired
+    private Sender sender;
+
+    @Autowired
+    private Receiver receiver;
+    private static String BOOT_TOPIC = "chatting";
+
+    @MessageMapping("/message")
+    public void sendMessage(ChattingMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        sender.send(BOOT_TOPIC, message.getMessage() + "|" + message.getUser());
+    }
+
+    @MessageMapping("/file")
+    @SendTo("/topic/chatting")
+    public ChattingMessage sendFile( ChattingMessage message) throws Exception {
+        return new ChattingMessage(message.getFileName(), message.getRawData(), message.getUser());
+    }
+}
